77246ea68b7c65d2ddfd48321962a0b1efc77c4b
HasanthaRukshan <66429685+HasanthaRukshan@users.noreply.github.com>
2022-10-22 10:17:47 +0530

    update the app js file

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 
 const userRoutes = require('./routes/user')
 const productRoutes = require('./routes/product')
+const sectionsRoutes = require('./routes/sections')
 // here is the routes for the application
 app.use('/app', userRoutes)
 app.use('/app', productRoutes)
+app.use('/app', sectionsRoutes)
 
 const port = 4500
 
