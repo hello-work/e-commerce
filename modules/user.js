3238db7ec8f833785403994e1edcb29bcb7c437a
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-10-22 13:06:45 +0530

    user module

---
+++ modules/user.js
@@ -0,0 +1,23 @@
+exports.login = async(req,res)=>{
+    const {email, password} = req.body
+
+    // TODO : database call
+    let user;
+    if(!user){
+        res.status(404).send({msg:'No user found', data:null})
+    }
+
+    res.status(200).send({msg:'Successful',  data:user})
+}
+
+exports.register = async(req,res)=>{
+    const {email, password} = req.body
+
+    // TODO : create user
+    let user;
+    if(!user){
+        res.status(404).send({msg:'No user found', data:null})
+    }
+
+    res.status(200).send({msg:'Successful',  data:user})
+}
