dad90236bca9511ec54991b79d8d81dae698b4e1
sudharaD <72960779+sudharaD@users.noreply.github.com>
2022-10-29 19:33:11 +0530

    change app.js to work with router

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const userRoutes = require('./routes/user')
 const productRoutes = require('./routes/product')
 const sectionsRoutes = require('./routes/sections')
+const cartRoutes = require('./routes/cart')
+
 // here is the routes for the application
 app.use('/app', userRoutes)
 app.use('/app', productRoutes)
